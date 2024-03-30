1afc901705a361ad54dc6861bb0f379bacf6d352
voin5991 <76397893+voin5991@users.noreply.github.com>
2024-03-30 12:15:55 +0500

    Project init

---
+++ app.py
@@ -0,0 +1,30 @@
+import asyncio
+import os
+
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import CommandStart
+
+from dotenv import find_dotenv, load_dotenv
+load_dotenv(find_dotenv())
+
+from handlers.user_private import user_private_router
+from common.bot_cmds_list import private
+from handlers.user_group import user_group_router
+
+ALLOWED_UPDATES = ['message, edited_message']
+
+bot = Bot(token=os.getenv('Token'))
+dp = Dispatcher()
+
+dp.include_router(user_private_router)
+dp.include_router(user_group_router)
+
+
+
+async def main():
+    await bot.delete_webhook(drop_pending_updates=True)
+    # await bot.delete_my_commands(scope=types.BotCommandScopeAllPrivateChats())
+    await bot.set_my_commands(commands=private, scope=types.BotCommandScopeAllPrivateChats())
+    await dp.start_polling(bot, allowed_updates=ALLOWED_UPDATES)
+
+asyncio.run(main())
